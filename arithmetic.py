9264cf77965d87095a0e85fe0aae2110bc758947
thuydung741 <68666285+thuydung741@users.noreply.github.com>
2022-04-09 20:02:59 -0500

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    plus = a + b
+    minus = a - b
+    product = a * b
+    print(plus)
+    print(minus)
+    print(product)
